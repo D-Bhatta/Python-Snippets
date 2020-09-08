939ab1ff19cae48e0070442193da69a308a7c806
D-Bhatta <dbhatta1232@gmail.com>
2020-09-08 09:29:21 -0400

    Add initial files

---
+++ programs/lan_client/client.py
@@ -0,0 +1,4 @@
+"""
+The client sends messages to the server, with input from the user, and prints
+the received message.
+"""
---
+++ programs/lan_client/server.py
@@ -0,0 +1,5 @@
+"""
+A server script.The server waits for a message from the client and prints
+it. On receiving the message ‘q’ it quits. It takes input from the user and
+sends it to the client.
+"""
